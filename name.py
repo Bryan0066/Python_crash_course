0883a0cd7f126afc322eb36129cb164a57aadcf9
Bryan0066 <38561548+Bryan0066@users.noreply.github.com>
2020-07-22 16:52:09 -0400

    Update name.py

--- name.py
+++ name.py
@@ -1,4 +1,30 @@
+# This file covers the basic projects in the beginning of the book, just reviewing
+
 name = "ada lovelace"
 print(name.title())
 print(name.upper())
 print(name.lower())
+print()
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+print()
+
+print(f'Hello,{full_name.title()}!')
+print()
+
+print('python')
+print('\tpython')
+print("Languages:\nPython\nC\nJavaScript")
+print()
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+print()
+
+favorite_language = ' python '
+favorite_language = favorite_language.rstrip()
+favorite_language = favorite_language.lstrip()
+print(favorite_language)
+print()
+
