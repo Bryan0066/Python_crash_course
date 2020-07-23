ce2fa3b313e2cd7646508aade68fa3345f1e864e
Bryan0066 <bgenest96@gmail.com>
2020-07-22 22:21:56 -0400

    Update name.py
    finishing up first set of problems

--- name.py
+++ name.py
@@ -3,30 +3,25 @@
 name = "ada lovelace"
 print(name.title())
 print(name.upper())
-print(name.lower())
-print()
+print(name.lower(),"\n")
 
 first_name = "ada"
 last_name = "lovelace"
 full_name = f"{first_name} {last_name}"
-print(full_name)
-print()
+print(full_name,'\n')
 
-print(f'Hello,{full_name.title()}!')
-print()
+print(f'Hello,{full_name.title()}!''\n')
 
 print('python')
 print('\tpython')
 print("Languages:\nPython\nC\nJavaScript")
 print()
-print("Languages:\n\tPython\n\tC\n\tJavaScript")
-print()
+print("Languages:\n\tPython\n\tC\n\tJavaScript\n")
 
 favorite_language = ' python '
 favorite_language = favorite_language.rstrip()
 favorite_language = favorite_language.lstrip()
-print(favorite_language)
-print()
+print(favorite_language, '\n')
 
 name = "eric"
 name = name.title()
@@ -36,7 +31,8 @@ print(name.upper())
 print(name.lower())
 
 famous_persons_name = "I'm famous"
-print(f"\nthis is my quote,\t\n it's very profound,\t\n you're welcome\n\n\t{famous_persons_name} ")
+print(f"\nthis is my quote,\t\n it's very profound,\t\n you're welcome\n\n\t-{famous_persons_name}\n")
+
 
 
 
