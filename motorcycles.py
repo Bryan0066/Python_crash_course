e5f8ed5bdd7e2eac32bd17de9babc7674860dafc
Bryan0066 <bgenest96@gmail.com>
2020-07-23 12:34:27 -0400

    Update motorcycles.py

---
+++ motorcycles.py
@@ -0,0 +1,17 @@
+
+motorcycles = ['yamaha', 'honda', "suzuki"]
+
+print(motorcycles)
+
+motorcycles[0] = 'ducati'
+
+motorcycles.append("ducati")
+
+
+
+print(motorcycles)
+
+motorcycles_1 = []
+
+motorcycles_1.append("yamaha")
+motorcycles_1.append("honda")
