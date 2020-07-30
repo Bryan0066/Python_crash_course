66c487f9d14d15bb8bb097700d8d32bdd424bcbe
Bryan0066 <bgenest96@gmail.com>
2020-07-30 16:29:59 -0400

    k

---
+++ first_numbers.py
@@ -0,0 +1,4 @@
+
+
+for value in range(1, 4):
+    print(value)
---
+++ sort.py
@@ -0,0 +1,25 @@
+# sorting a list
+
+cars = ["bmw", "audi", "toyota"]
+
+cars.sort()
+print(cars)
+
+cars.sort(reverse=True)
+print(cars, "")
+
+cars = ["bmw", "audi", "toyota"]
+print(cars, "\n")
+
+# temporarily sorting a list
+
+print(sorted(cars), "\n")
+
+print(cars)
+
+cars.reverse()
+
+print(cars)
+
+print(len(cars))
+
